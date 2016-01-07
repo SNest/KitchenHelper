c993423e59a56d8d5634df82d7900eadff257365
Sergey Nestertsov <sergii_nestertsov@epam.com>
2016-01-07 12:13:47 +0200

    initial commit

---
+++ KitchenHelperClient/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace KitchenHelperClient
+{
+    [Activity(Label = "KitchenHelperClient", MainLauncher = true, Icon = "@drawable/icon",
+        Theme = "@style/Theme.MyTheme")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
---
+++ KitchenHelperServer/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace KitchenHelperServer.Models
+{
+    public class Recipe : Entity
+    {
+        public string Name { get; set; }
+
+        public virtual ICollection<Ingredient> Ingredients { get; set; }
+
+        public virtual ICollection<RecipeStep> RecipeSteps { get; set; }
+    }
+}
